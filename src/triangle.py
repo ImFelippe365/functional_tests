2e0f7fd9566826ef704d0aed0926930d61ac9568
Felippe <felipperian365@gmail.com>
2023-10-27 13:08:26 -0300

    finalizado

--- src/triangle.py
+++ src/triangle.py
@@ -11,12 +11,12 @@ class Triangle:
     
     def all_sides_length(self):
         allSidesSum = self.total_length / 2
-        return (self.side1 < allSidesSum and self.side2 < allSidesSum and self.side3 < allSidesSum)
+        return (self.side1 < allSidesSum or self.side2 < allSidesSum or self.side3 < allSidesSum)
     
     def side_equals_sum_other(self):
         side1Sum = (self.total_length - self.side1)
         side2Sum = (self.total_length - self.side2)
         side3Sum = (self.total_length - self.side3)
         
-        return (self.side1 == side1Sum or self.side2 == side2Sum or self.side3 == side3Sum)
+        return (self.side1 != side1Sum or self.side2 != side2Sum or self.side3 != side3Sum)
     
--- tests/test_base.py
+++ tests/test_base.py
@@ -5,9 +5,9 @@ class TestBase:
 
     @pytest.fixture
     def setUp(self):
-        # triangle = Triangle(0,0,0)
-        # triangle = Triangle(10,5,0)
+        triangle = Triangle(0,0,0)
+        # triangle = Triangle(10,5,5)
         # triangle = Triangle(8,8,8)
-        triangle = Triangle(4,2,2)
+        # triangle = Triangle(4, 3, 3)
 
         return triangle
